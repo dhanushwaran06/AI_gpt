2de87e3958d93f830d200c35f964dfa05a89a5e0
dhanushwaran06 <ajdhanush.jvl@gmail.com>
2023-06-22 09:25:09 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+// import React from "react"
+import Demo from './components/Demo'
+import Hero from './components/Hero'
+
+
+import './app.css'
+
+const App = () => {
+  return (
+    <main>
+      <div className='main'>
+        <div className='gradient'/>
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  )
+}
+
+export default App
